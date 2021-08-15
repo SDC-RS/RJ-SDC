38155e594ee396ee2f86a38e648a7c5ace1910cc
rachel <jones.rachel.a464@gmail.com>
2021-08-14 19:46:41 -0700

    create queries and server routes

--- server/db.js
+++ server/db.js
@@ -8,9 +8,8 @@ const pool = new Pool({
   database: 'sdc',
   port: 5432,
 })
-
-const getProductById = (req, cb) => {
-  const query = `SELECT * FROM datasdc.products INNER JOIN datasdc.features ON datasdc.products.product_id = ${req.params.product_id} AND datasdc.features.product_id = ${req.params.product_id}`;
+const getAllProducts = (cb) => {
+  const query = 'SELECT * FROM datasdc.products LIMIT 5'
   pool.query(query, null, (err, results) => {
     if (err) {
     cb(err);
@@ -19,6 +18,78 @@ const getProductById = (req, cb) => {
   })
 }
 
+const getProductById = (productId, cb) => {
+  const query = `SELECT * FROM datasdc.products INNER JOIN datasdc.features \
+   ON datasdc.products.product_id = datasdc.features.product_id WHERE \
+   datasdc.features.product_id = ${productId}`;
+  pool.query(query, null, (err, results) => {
+    if (err) {
+    cb(err);
+    }
+    const product = {
+      product_id: results.rows[0].product_id,
+      name: results.rows[0].name,
+      slogan: results.rows[0].slogan,
+      description: results.rows[0].description,
+      category: results.rows[0].category,
+      default_price: results.rows[0].default_price,
+      features: []
+    }
+    results.rows.forEach((row) => {
+      if (row.feature) {
+        const feature = {
+          feature: row.feature,
+          value: row.value
+        }
+        product.features.push(feature);
+      }
+    })
+    cb(null, product);
+  })
+}
+
+const getRelatedProducts = (productId, cb) => {
+  const query = `SELECT * FROM datasdc.related WHERE \
+   datasdc.related.current_product_id = ${productId}`;
+  pool.query(query, null, (err, results) => {
+    if (err) {
+    cb(err);
+    }
+    const related = [];
+    results.rows.forEach((row) => {
+      related.push(row.related_product_id)
+    })
+    cb(null, related);
+  })
+}
+
+const getStyles = (productId, cb) => {
+  const query1 = `SELECT s.*, ARRAY_AGG(json_build_object('thumbnail_url', p.thumbnail_url, 'url', \
+  p.url)) AS photos FROM datasdc.styles s INNER JOIN datasdc.photos p ON s.styles_id = p.styles_id \
+  WHERE s.product_id = ${productId} GROUP BY s.styles_id;`;
+  const query2 = `SELECT s.styles_id, json_object_agg(sk.skus_id, json_build_object('size', sk.size, \
+  'quantity', sk.quantity)) AS skus FROM datasdc.styles s INNER JOIN datasdc.skus sk ON s.styles_id = sk.styles_id \
+  WHERE s.product_id = ${productId} GROUP BY s.styles_id;`
+  pool.query(query1, null, (err, results) => {
+    if (err) {
+    cb(err);
+    }
+    let styles = results.rows;
+    pool.query(query2, null, (err, results) => {
+      if (err) {
+      cb(err);
+      }
+      for (let i = 0; i < results.rows.length; i++) {
+        styles[i].skus = results.rows[i].skus;
+      }
+      cb(null, styles);
+    })
+  })
+}
+
 module.exports = {
-  getProductById
+  getProductById,
+  getRelatedProducts,
+  getAllProducts,
+  getStyles
 }
--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const port = 3020;
 
 app.use(express.json());
 
-app.get('/products/:product_id', (req, res) => {
-  db.getProductById(req, (err, data) => {
+app.get('/products', (req, res) => {
+  db.getAllProducts((err, data) => {
     if (err) {
       res.sendStatus(500).end('could not get product', err.stack)
     }
@@ -17,7 +17,25 @@ app.get('/products/:product_id', (req, res) => {
 })
 
 app.get('/products/:product_id/', (req, res) => {
-  db.getProductById(req, (err, data) => {
+  db.getProductById(req.params.product_id, (err, data) => {
+    if (err) {
+      res.sendStatus(500).end('could not get product', err.stack)
+    }
+    res.send(data);
+  })
+})
+
+app.get('/products/:product_id/related', (req, res) => {
+  db.getRelatedProducts(req.params.product_id, (err, data) => {
+    if (err) {
+      res.sendStatus(500).end('could not get product', err.stack)
+    }
+    res.send(data);
+  })
+})
+
+app.get('/products/:product_id/styles', (req, res) => {
+  db.getStyles(req.params.product_id, (err, data) => {
     if (err) {
       res.sendStatus(500).end('could not get product', err.stack)
     }
